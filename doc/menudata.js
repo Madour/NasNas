eb7276c8f2d08dcb77a6889dee6da44ab016a016
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-13 19:50:47 +0000

    Deploy Madour/NasNas to github.com/Madour/NasNas.git:gh-pages

--- doc/menudata.js
+++ doc/menudata.js
@@ -31,6 +31,7 @@ var menudata={children:[
 {text:"a",url:"functions_func.html#index_a"},
 {text:"b",url:"functions_func.html#index_b"},
 {text:"c",url:"functions_func.html#index_c"},
+{text:"d",url:"functions_func.html#index_d"},
 {text:"f",url:"functions_func.html#index_f"},
 {text:"g",url:"functions_func.html#index_g"},
 {text:"h",url:"functions_func.html#index_h"},
