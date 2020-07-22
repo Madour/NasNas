a7c5864a35b364902868539725931b8244080305
Madour <modar1999@gmail.com>
2020-07-22 22:40:12 +0200

    Organized cmake files Splitted into multiples cmake modules

---
+++ cmake/Data.cmake
@@ -0,0 +1,24 @@
+set(
+
+        NasNas_Src
+
+        ${NasNas_Src}
+
+        ../src/NasNas/data/AppComponent.cpp
+        ../src/NasNas/data/Config.cpp
+        ../src/NasNas/data/Arial.cpp
+
+)
+
+set(
+        NasNas_Inc
+
+        ${NasNas_Inc}
+
+        ../include/NasNas/data/AppComponent.hpp
+        ../include/NasNas/data/Arial.hpp
+        ../include/NasNas/data/Config.hpp
+        ../include/NasNas/data/Drawable.hpp
+        ../include/NasNas/data/Rect.hpp
+
+)
---
+++ cmake/Reslib.cmake
@@ -0,0 +1,19 @@
+set(
+
+        NasNas_Src
+
+        ${NasNas_Src}
+
+        ../src/NasNas/reslib/ResourceLoader.cpp
+        ../src/NasNas/reslib/ResourceManager.cpp
+
+)
+
+set(
+        NasNas_Inc
+
+        ${NasNas_Inc}
+
+        ../include/NasNas/reslib/ResourceLoader.hpp
+        ../include/NasNas/reslib/ResourceManager.hpp
+)
