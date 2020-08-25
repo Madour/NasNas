5e898afb9db271e2433c0672aed6862efeb1f76b
Madour <modar1999@gmail.com>
2020-08-25 18:02:58 +0200

    added support for LineShape

--- include/NasNas/data/Shapes.hpp
+++ include/NasNas/data/Shapes.hpp
@@ -6,6 +6,7 @@
 
 #include <SFML/Graphics.hpp>
 #include "NasNas/data/Maths.hpp"
+#include "NasNas/data/Drawable.hpp"
 
 namespace sf {
     class EllipseShape : public Shape {
@@ -27,5 +28,22 @@ namespace sf {
     private :
         std::size_t m_point_cout = 30;
         Vector2f m_radius;
+    };
+    class LineShape : public ns::Drawable, public Transformable {
+    public:
+        LineShape();
+        void addPoint(float x, float y);
+        void addPoint(const Vector2f& point);
+        void removePoint(unsigned int index);
+
+        void setColor(const Color& color);
+        void setColor(unsigned int index, const Color& color);
+
+        auto getPosition() -> Vector2f override;
+        auto getGlobalBounds() -> ns::FloatRect override;
+
+    private:
+        void draw(RenderTarget& target, RenderStates states) const override;
+        std::vector<Vertex> m_vertices;
     };
 }
