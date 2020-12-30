0989acb47dc2cdc8de0bfebdf337c6f5a562431f
Madour <modar1999@gmail.com>
2020-12-30 16:11:28 +0100

    set texture for vertex array

--- include/NasNas/data/VertexArray.hpp
+++ include/NasNas/data/VertexArray.hpp
@@ -11,11 +11,16 @@ namespace ns {
 
     class VertexArray : public ns::Drawable, public sf::VertexArray {
     public:
+        void setTexture(const sf::Texture& texture) {
+            m_texture = &texture;
+        }
         auto getPosition() const -> sf::Vector2f override;
         auto getGlobalBounds() const -> ns::FloatRect override;
 
     private:
+        const sf::Texture* m_texture = nullptr;
         void draw(sf::RenderTarget &target, sf::RenderStates states) const override {
+            states.texture = m_texture;
             target.draw(static_cast<sf::VertexArray>(*this), states);
         }
     };
