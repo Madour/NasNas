96832a3915952181f88d0fec9c8e92006c210ea5
Modar Nasser <modar1999@gmail.com>
2021-01-20 00:42:07 +0100

    huge performance optimization in SpriteBatch improved spritebatch example

--- include/NasNas/core/SpriteBatch.hpp
+++ include/NasNas/core/SpriteBatch.hpp
@@ -13,19 +13,30 @@
 namespace ns {
 
     class SpriteBatch : public ns::Drawable {
-    public:
 
+        struct SpriteBatchLayer {
+            explicit SpriteBatchLayer(const sf::Texture* tex);
+            const sf::Texture* texture;
+            sf::VertexBuffer buffer;
+            std::vector<sf::Sprite*> sprites;
+            std::vector<sf::Vertex> vertices;
+        };
+
+    public:
         explicit SpriteBatch(std::string name);
         ~SpriteBatch() override;
 
         void clear();
 
-        void draw(sf::Sprite* sprite);
+        void start(sf::VertexBuffer::Usage usage);
 
+        void draw(sf::Sprite* sprite);
         void draw(sf::Texture* texture, sf::Vector2f pos, sf::IntRect rect);
 
         void end();
 
+        auto getDepth() const -> int;
+        auto getUsage() const -> sf::VertexBuffer::Usage;
         auto getPosition() const -> sf::Vector2f override;
         auto getGlobalBounds() const -> ns::FloatRect override;
 
@@ -33,11 +44,13 @@ namespace ns {
         friend App;
         void render();
         void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+
         std::string m_name;
-        std::vector<sf::Sprite*> m_owned_sprites;
-        std::vector<std::vector<sf::Sprite*>> m_sprites;
-        std::vector<std::pair<const sf::Texture*, std::vector<sf::Vertex>>> m_vertices;
         ns::FloatRect m_global_bounds;
+        sf::VertexBuffer::Usage m_usage;
+
+        std::vector<SpriteBatchLayer> m_layers;
+        std::vector<sf::Sprite*> m_owned_sprites;
 
         static std::vector<SpriteBatch*> list;
     };
--- src/NasNas/core/SpriteBatch.cpp
+++ src/NasNas/core/SpriteBatch.cpp
@@ -7,27 +7,34 @@
 using namespace ns;
 
 
+SpriteBatch::SpriteBatchLayer::SpriteBatchLayer(const sf::Texture* tex) :
+texture(tex)
+{}
+
 std::vector<SpriteBatch*> SpriteBatch::list;
 
 SpriteBatch::SpriteBatch(std::string name) :
-m_name(std::move(name)) {
+m_name(std::move(name)),
+m_usage(sf::VertexBuffer::Usage::Stream) {
     SpriteBatch::list.push_back(this);
     clear();
 }
 
 SpriteBatch::~SpriteBatch() {
     SpriteBatch::list.erase(std::find(SpriteBatch::list.begin(), SpriteBatch::list.end(), this));
-    for (auto& spr : m_owned_sprites)
+    for (auto* spr : m_owned_sprites)
         delete(spr);
 }
 
+void SpriteBatch::start(sf::VertexBuffer::Usage usage) {
+    m_usage = usage;
+}
+
 void SpriteBatch::draw(sf::Sprite* sprite) {
-    if (m_sprites.empty())
-        m_sprites.emplace_back();
-    if (!m_sprites[m_sprites.size()-1].empty())
-            if (m_sprites[m_sprites.size()-1][0]->getTexture() != sprite->getTexture())
-                m_sprites.emplace_back();
-    m_sprites[m_sprites.size()-1].push_back(sprite);
+    if (m_layers.empty() || m_layers.back().texture != sprite->getTexture()) {
+        m_layers.emplace_back(sprite->getTexture());
+    }
+    m_layers.back().sprites.push_back(sprite);
 }
 
 void SpriteBatch::draw(sf::Texture* texture, sf::Vector2f pos, sf::IntRect rect) {
@@ -39,18 +46,29 @@ void SpriteBatch::draw(sf::Texture* texture, sf::Vector2f pos, sf::IntRect rect)
 }
 
 void SpriteBatch::end() {
-    for (unsigned int i = 0; i < m_sprites.size(); ++i) {
-        m_vertices.emplace_back();
-        m_vertices[i].second.reserve(m_sprites[i].size()*6);
+    for (auto& layer : m_layers) {
+        layer.vertices.resize(layer.sprites.size()*6);
+        layer.buffer.create(layer.sprites.size()*6);
+        layer.buffer.setUsage(m_usage);
+        layer.buffer.setPrimitiveType(sf::PrimitiveType::Triangles);
     }
+    render();
 }
 
 void SpriteBatch::clear() {
-    m_vertices.clear();
-    m_sprites.clear();
-    for (auto& spr : m_owned_sprites)
+    for (auto* spr : m_owned_sprites)
         delete(spr);
+    m_owned_sprites.clear();
+    m_layers.clear();
     m_global_bounds = {0, 0, 0, 0};
+}
+
+auto SpriteBatch::getDepth() const -> int {
+    return m_layers.size();
+}
+
+auto SpriteBatch::getUsage() const -> sf::VertexBuffer::Usage {
+    return m_usage;
 }
 
 auto SpriteBatch::getPosition() const -> sf::Vector2f {
@@ -62,31 +80,44 @@ auto SpriteBatch::getGlobalBounds() const -> ns::FloatRect {
 }
 
 void SpriteBatch::render() {
-    for (auto& [texture, vector] : m_vertices)
-        vector.clear();
-    m_vertices.clear();
-
-    for (unsigned int order = 0; order < m_sprites.size() ; ++order) {
-        auto& sprites = m_sprites[order];
-        if (!sprites.empty())
-            m_vertices.push_back({sprites[0]->getTexture(), {}});
-        for (unsigned int i = 0; i < sprites.size(); ++i) {
-            auto& spr = sprites[i];
+    bool first = true;
+    for (auto& layer : m_layers) {
+        for (unsigned int i = 0; i < layer.sprites.size(); ++i) {
+            auto& spr = layer.sprites[i];
             const ns::FloatRect tex_rect{spr->getTextureRect()};
             const ns::FloatRect lb{spr->getLocalBounds()};
             const ns::FloatRect rect{spr->getTransform().transformRect(lb)};
+
             const auto& topleft = spr->getTransform().transformPoint(lb.topleft());
             const auto& topright = spr->getTransform().transformPoint(lb.topright());
             const auto& bottomright = spr->getTransform().transformPoint(lb.bottomright());
             const auto& bottomleft = spr->getTransform().transformPoint(lb.bottomleft());
-            m_vertices[order].second.emplace_back(topleft, spr->getColor(), tex_rect.topleft());
-            m_vertices[order].second.emplace_back(bottomright, spr->getColor(), tex_rect.bottomright());
-            m_vertices[order].second.emplace_back(bottomleft, spr->getColor(), tex_rect.bottomleft());
-            m_vertices[order].second.emplace_back(topleft, spr->getColor(), tex_rect.topleft());
-            m_vertices[order].second.emplace_back(topright, spr->getColor(), tex_rect.topright());
-            m_vertices[order].second.emplace_back(bottomright, spr->getColor(), tex_rect.bottomright());
-            if (i == 0 && order == 0)
+
+            layer.vertices[i*6+0].position = topleft;
+            layer.vertices[i*6+1].position = bottomright;
+            layer.vertices[i*6+2].position = bottomleft;
+            layer.vertices[i*6+3].position = topleft;
+            layer.vertices[i*6+4].position = topright;
+            layer.vertices[i*6+5].position = bottomright;
+
+            layer.vertices[i*6+0].color = spr->getColor();
+            layer.vertices[i*6+1].color = spr->getColor();
+            layer.vertices[i*6+2].color = spr->getColor();
+            layer.vertices[i*6+3].color = spr->getColor();
+            layer.vertices[i*6+4].color = spr->getColor();
+            layer.vertices[i*6+5].color = spr->getColor();
+
+            layer.vertices[i*6+0].texCoords = tex_rect.topleft();
+            layer.vertices[i*6+1].texCoords = tex_rect.bottomright();
+            layer.vertices[i*6+2].texCoords = tex_rect.bottomleft();
+            layer.vertices[i*6+3].texCoords = tex_rect.topleft();
+            layer.vertices[i*6+4].texCoords = tex_rect.topright();
+            layer.vertices[i*6+5].texCoords = tex_rect.bottomright();
+
+            if (first) {
                 m_global_bounds = rect;
+                first = false;
+            }
             else {
                 m_global_bounds.left = std::min(rect.left, m_global_bounds.left);
                 m_global_bounds.top = std::min(rect.top, m_global_bounds.top);
@@ -96,12 +127,13 @@ void SpriteBatch::render() {
                 m_global_bounds.height = bottom - m_global_bounds.top;
             }
         }
+        layer.buffer.update(layer.vertices.data());
     }
 }
 
 void SpriteBatch::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    for (const auto& [texture, vector] : m_vertices) {
-        states.texture = texture;
-        target.draw(vector.data(), vector.size(), sf::PrimitiveType::Triangles, states);
+    for (const auto& layer : m_layers) {
+        states.texture = layer.texture;
+        target.draw(layer.buffer, states);
     }
 }
