fd13ba6a571865051513f331f8dd501770fddecc
Modar Nasser <modar1999@gmail.com>
2021-02-03 20:09:56 +0100

    scale can be float

--- examples/split_view/main.cpp
+++ examples/split_view/main.cpp
@@ -13,31 +13,34 @@
  */
 class Game : public ns::App {
 public:
-    Game() : ns::App("Split View", 1080, 720) {
+    Game() : ns::App("Split View", {1080*2, 720*2}, 0.5f) {
         // create a scene
         auto* scene = createScene("main");
 
+        sf::Vector2i resolution{getWindow().getAppView().getSize()};
+        sf::Vector2i window_size{getWindow().getSize()};
+
         // adding some shapes to the scene
-        auto* bg = new sf::RectangleShape({1080, 720});
+        auto* bg = new sf::RectangleShape(getWindow().getAppView().getSize());
         bg->setFillColor(sf::Color::White);
         scene->getDefaultLayer()->add(bg);
 
-        for (int i = 0; i < 50; ++i) {
+        for (int i = 0; i < 100; ++i) {
             auto* shape = new sf::CircleShape(20);
             shape->setOrigin(20, 20);
             shape->setPointCount(std::rand()%5+3);
             shape->setFillColor(sf::Color(std::rand()%25 * 10, std::rand()%25 * 10, std::rand()%25 * 10, 255));
-            shape->setPosition(std::rand()%1080, std::rand()%720);
+            shape->setPosition(std::rand()%resolution.x, std::rand()%resolution.y);
             shape->setOutlineColor(sf::Color::Black);
             shape->setOutlineThickness(1);
             scene->getDefaultLayer()->add(shape);
         }
 
         // creating 2 cameras each taking half the screen (viewport width of 0.499 to let a black line separator)
-        auto* camera_left = createCamera("left", 0, {0, 0, 1080/2, 720}, {0, 0, 0.499, 1});
+        auto* camera_left = createCamera("left", 0, {0, 0, window_size.x/2, window_size.y}, {0, 0, 0.499, 1});
         camera_left->lookAt(scene);
 
-        auto* camera_right = createCamera("right", 0, {0, 0, 1080/2, 720}, {0.501, 0, 0.499, 1});
+        auto* camera_right = createCamera("right", 0, {0, 0, window_size.x/2, window_size.y}, {0.501, 0, 0.499, 1});
         camera_right->lookAt(scene);
 
         addDebugText("Press WASD to move left camera or arrow keys to move right camera.", {50, 10});
@@ -69,6 +72,7 @@ public:
 int main() {
     ns::Config::debug = true;
     ns::Config::debug.show_bounds = false;
+    ns::Config::Window::antialiasing = 0;
     Game g;
     g.run();
     return 0;
