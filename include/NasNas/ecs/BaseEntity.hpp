500402c62fc2eb5ba00593692c74b33449473424
Madour <modar1999@gmail.com>
2021-02-04 17:18:55 +0100

    warning fix

--- include/NasNas/ecs/BaseEntity.hpp
+++ include/NasNas/ecs/BaseEntity.hpp
@@ -22,4 +22,44 @@ namespace ns {
 
     };
 
+    template<class T, typename... TArgs>
+    void BaseEntity::addComponent(TArgs... component_args) {
+        addComponent(std::make_shared<T>(this, std::forward<TArgs>(component_args)...));
+    }
+
+    template<typename T>
+    void BaseEntity::addComponent(std::shared_ptr<T> new_component) {
+        static_assert(std::is_base_of_v<ecs::BaseComponent, T>, "New component type must be derived from BaseComponent.");
+
+        if constexpr (std::is_base_of_v<ecs::GraphicsComponent, T>)
+            m_graphics_components_list.push_back(new_component.get());
+        else if constexpr (std::is_same_v<ecs::PhysicsComponent, T>)
+            m_physics_component = new_component.get();
+        else if constexpr (std::is_same_v<ecs::InputsComponent, T>)
+            m_inputs_component = new_component.get();
+        else if constexpr (std::is_same_v<ecs::ColliderComponent, T>)
+            m_collider_component = new_component.get();
+
+        m_components_list.push_back(new_component);
+    }
+
+    template<typename T>
+    void BaseEntity::addComponent(T* new_component) {
+        addComponent(std::shared_ptr<T>(new_component));
+    }
+
+    template <typename T>
+    auto BaseEntity::graphics(unsigned int index) -> T* {
+        if (index < m_graphics_components_list.size()) {
+            auto ptr = dynamic_cast<T*>(m_graphics_components_list[index]);
+            if (ptr != nullptr)
+                return ptr;
+            std::cout << "Entity «" << m_name << "»'s graphic component number " << index << " is not of type "
+                      << typeid(T).name() << std::endl;
+            exit(-1);
+        }
+        std::cout << "Entity «" << m_name << "» has less than " << index << " graphics components. Index out of range." << std::endl;
+        exit(-1);
+    }
+
 }
