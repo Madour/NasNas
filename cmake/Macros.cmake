7201e45827e8fc8c957f388d774dd109b7772204
Modar Nasser <modar1999@gmail.com>
2021-02-11 23:08:05 +0100

    can now choose to build SFML or not

---
+++ cmake/Macros.cmake
@@ -0,0 +1,43 @@
+set(NasNas_Src "")
+set(NasNas_Inc "")
+set(NasNas_Libs "")
+
+macro(find_SFML)
+    set(SFML_FOUND FALSE)
+    if(NASNAS_FIND_SFML)
+        find_package(SFML 2 COMPONENTS graphics audio QUIET)
+    endif()
+
+    if (SFML_FOUND)
+        message(STATUS "Found SFML in ${SFML_DIR}")
+    else ()
+        set(BUILD_SHARED_LIBS FALSE)
+        set(SFML_USE_STATIC_STD_LIBS FALSE)
+        mark_as_advanced(BUILD_SHARED_LIBS)
+        add_subdirectory(SFML)
+
+        if(MSVC AND SFML_USE_STATIC_STD_LIBS)
+            foreach(flag
+                    CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_DEBUG CMAKE_CXX_FLAGS_RELEASE
+                    CMAKE_CXX_FLAGS_MINSIZEREL CMAKE_CXX_FLAGS_RELWITHDEBINFO)
+                if(${flag} MATCHES "/MD")
+                    string(REGEX REPLACE "/MD" "/MT" ${flag} "${${flag}}")
+                endif()
+            endforeach()
+        endif()
+        message(STATUS "Found SFML 2.5.1 in ${CMAKE_CURRENT_SOURCE_DIR}/SFML")
+    endif()
+    set(NasNas_Libs "${NasNas_Libs};sfml-graphics;sfml-audio")
+endmacro()
+
+macro(find_NasNas)
+    include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/NasNas.cmake)
+endmacro()
+
+macro(check_compiler)
+    if(NOT WIN32)
+        if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+            set(${NasNas_Libs} "${NasNas_Libs};m;stdc++fs;stdc++")
+        endif()
+    endif()
+endmacro()
