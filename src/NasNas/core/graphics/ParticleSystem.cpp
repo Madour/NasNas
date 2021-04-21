37ea57fa0be63150f6868b42cb0f91746ae71e59
Modar Nasser <modar1999@gmail.com>
2021-04-21 23:10:52 +0200

    implement particles system emmit rate

--- src/NasNas/core/graphics/ParticleSystem.cpp
+++ src/NasNas/core/graphics/ParticleSystem.cpp
@@ -8,11 +8,16 @@ void ParticleSystem::setTexture(const sf::Texture& texture) {
     m_texture = &texture;
 }
 
+void ParticleSystem::setEmmitRate(float rate) {
+    m_rate = rate;
+}
+
 void ParticleSystem::emmit(const sf::IntRect& rect, int nb, bool repeat) {
     for (int i = 0; i < nb; ++i) {
         m_particles.emplace_back();
         auto& particle = m_particles.back();
         onParticleCreate(particle);
+        particle.lifetime = 0.f;
         particle.clock.restart();
         particle.repeat = repeat;
         particle.sprite.setTexture(*m_texture);
@@ -43,31 +48,37 @@ auto ParticleSystem::getGlobalBounds() const -> ns::FloatRect {
 }
 
 void ParticleSystem::update() {
+    auto elapsed = m_clk.getElapsedTime().asSeconds();
+    m_to_emmit += elapsed*m_rate;
+
+    m_batch.clear();
     for (auto it = m_particles.begin(); it != m_particles.end();) {
         auto& particle = *it;
-        onParticleUpdate(particle);
-        particle.sprite.move(particle.velocity);
-        particle.sprite.setScale(particle.scale, particle.scale);
-        particle.sprite.setRotation(particle.rotation);
-        particle.sprite.setColor(particle.color);
-        if (particle.getAge() > particle.lifetime) {
-            if (particle.repeat) {
-                particle.sprite.setPosition(m_position);
-                particle.clock.restart();
-                onParticleCreate(particle);
-                it++;
-            }
-            else {
-                it = m_particles.erase(it);
-            }
+        if (particle.getAge() < particle.lifetime) {
+            onParticleUpdate(particle);
+            particle.sprite.move(particle.velocity);
+            particle.sprite.setScale(particle.scale, particle.scale);
+            particle.sprite.setRotation(particle.rotation);
+            particle.sprite.setColor(particle.color);
+            m_batch.draw(&particle.sprite);
+            it++;
+            continue;
         }
-        else it++;
-    }
-    m_batch.clear();
-    for (auto& particle : m_particles) {
-        m_batch.draw(&particle.sprite);
+        if (particle.repeat || particle.lifetime == 0.f) {
+            if (m_to_emmit > 1.f) {
+                m_to_emmit -= 1.f;
+                onParticleCreate(particle);
+                particle.clock.restart();
+            }
+            particle.sprite.setPosition(m_position);
+            it++;
+        }
+        else {
+            it = m_particles.erase(it);
+        }
     }
     m_batch.end();
+    m_clk.restart();
 }
 
 void ParticleSystem::draw(sf::RenderTarget& target, sf::RenderStates states) const {
