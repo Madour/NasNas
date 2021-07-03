49dc86f68fcef3b5ef15bea444742f154f37e042
Modar Nasser <modar1999@gmail.com>
2021-07-03 23:17:06 +0200

    add include header for each module

--- include/NasNas.h
+++ include/NasNas.h
@@ -5,43 +5,20 @@
 #pragma once
 
 
-#include "NasNas/core/App.hpp"
-#include "NasNas/core/Camera.hpp"
-#include "NasNas/core/Scene.hpp"
-#include "NasNas/core/Layer.hpp"
-#include "NasNas/core/Window.hpp"
-#include "NasNas/core/Debug.hpp"
-
-#include "NasNas/core/data/Config.hpp"
-#include "NasNas/core/data/Maths.hpp"
-#include "NasNas/core/data/Logger.hpp"
-#include "NasNas/core/data/Utils.hpp"
-
-#include "NasNas/core/graphics/BitmapText.hpp"
-#include "NasNas/core/graphics/ParticleSystem.hpp"
-#include "NasNas/core/graphics/Shapes.hpp"
-#include "NasNas/core/graphics/Sprite.hpp"
-#include "NasNas/core/graphics/SpriteBatch.hpp"
-#include "NasNas/core/graphics/VertexArray.hpp"
+#include "NasNas/Core.hpp"
 
 #ifdef NS_ECS
-#include "NasNas/ecs/BaseEntity.hpp"
-#include "NasNas/ecs/SpriteComponent.hpp"
-#include "NasNas/ecs/ShapeComponent.hpp"
+#include "NasNas/Ecs.hpp"
 #endif
 
 #ifdef NS_TILEMAPPING
-#include "NasNas/tilemapping/TiledMap.hpp"
-#include "NasNas/tilemapping/TileLayer.hpp"
-#include "NasNas/tilemapping/ObjectLayer.hpp"
-#include "NasNas/tilemapping/GroupLayer.hpp"
+#include "NasNas/Tilemapping.hpp"
 #endif
 
 #ifdef NS_RESLIB
-#include "NasNas/reslib/ResourceManager.hpp"
+#include "NasNas/Reslib.hpp"
 #endif
 
 #ifdef NS_UI
-#include "NasNas/ui/TypedText.hpp"
+#include "NasNas/Ui.hpp"
 #endif
-
---
+++ include/NasNas/Reslib.hpp
@@ -0,0 +1,5 @@
+// Created by Modar Nasser on 03/07/2021.
+
+#pragma once
+
+#include "NasNas/reslib/ResourceManager.hpp"
