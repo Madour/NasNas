c50cec6f8622f0dae41d3f4412fff00165d56d1c
Madour <modar1999@gmail.com>
2021-07-08 09:30:49 +0200

    add examples to install exports

--- cmake/NasNasUtils.cmake
+++ cmake/NasNasUtils.cmake
@@ -1,4 +1,13 @@
+include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
 set(NasNas_Libs "")
+
+# global list that contains all NasNas targets
+define_property(GLOBAL
+    PROPERTY NASNAS_TARGETS
+    BRIEF_DOCS "Contains all available targets defined by NasNas"
+    FULL_DOCS "Contains all available targets defined by NasNas"
+)
 
 macro(find_SFML)
     if(NASNAS_FIND_SFML)
@@ -6,7 +15,7 @@ macro(find_SFML)
     endif()
 
     if (SFML_FOUND)
-        message(STATUS "Found SFML in ${SFML_DIR}")
+        message(STATUS "Found SFML 2 in ${SFML_DIR}")
     else ()
         set(BUILD_SHARED_LIBS FALSE)
         set(SFML_USE_STATIC_STD_LIBS FALSE)
@@ -34,6 +43,12 @@ macro(check_compiler)
         endif()
     endif()
 endmacro()
+
+function(NasNas_register_target name)
+    get_property(NasNas_Targets GLOBAL PROPERTY NASNAS_TARGETS)
+    list(APPEND NasNas_Targets ${name})
+    set_property(GLOBAL PROPERTY NASNAS_TARGETS ${NasNas_Targets})
+endfunction()
 
 function(NasNas_add_module name src inc)
     message("   -> ${name}")
@@ -67,8 +82,49 @@ function(NasNas_add_module name src inc)
 
     add_library(NasNas::${name} ALIAS ${target})
 
-    get_property(NasNas_Targets GLOBAL PROPERTY NASNAS_TARGETS)
-    list(APPEND NasNas_Targets ${target})
-    set_property(GLOBAL PROPERTY NASNAS_TARGETS ${NasNas_Targets})
+    install(TARGETS ${target} EXPORT NasNasTargets ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR})
+
+    NasNas_register_target(${target})
 endfunction()
 
+function(NasNas_add_example name src inc)
+    set(target ${name})
+
+    add_executable(${target} "${src};${inc}")
+    target_include_directories(${target} PUBLIC $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>)
+    target_include_directories(${target} PUBLIC $<INSTALL_INTERFACE:include>)
+    target_link_libraries(${target} PRIVATE NasNas::Ecs NasNas::Reslib NasNas::Tilemapping NasNas::Ui)
+    set_target_properties(
+            ${target}
+            PROPERTIES
+            CXX_STANDARD 17
+            DEBUG_POSTFIX -d
+            RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/bin
+            RUNTIME_OUTPUT_DIRECTORY_DEBUG ${CMAKE_CURRENT_BINARY_DIR}/bin
+            RUNTIME_OUTPUT_DIRECTORY_RELEASE ${CMAKE_CURRENT_BINARY_DIR}/bin
+    )
+
+    install(TARGETS ${target} EXPORT NasNasTargets RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}/NasNas)
+
+    NasNas_register_target(${target})
+endfunction()
+
+function(NasNas_export_install)
+    # build tree export
+    export(TARGETS ${NasNas_Targets} FILE ${PROJECT_BINARY_DIR}/NasNas.cmake NAMESPACE NasNas::)
+
+    # generate NasNasConfig.cmake
+    configure_package_config_file(
+            "${PROJECT_SOURCE_DIR}/cmake/NasNasConfig.cmake.in" "${PROJECT_BINARY_DIR}/NasNasConfig.cmake"
+            INSTALL_DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/NasNas
+    )
+    # install NasNasConfig.cmake file
+    install(FILES ${PROJECT_BINARY_DIR}/NasNasConfig.cmake DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/NasNas)
+    # install license and readme
+    install(FILES ${PROJECT_SOURCE_DIR}/LICENSE.md DESTINATION ${CMAKE_INSTALL_DOCDIR})
+    install(FILES ${PROJECT_SOURCE_DIR}/README.md DESTINATION ${CMAKE_INSTALL_DOCDIR})
+    # install headers
+    install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/NasNas DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
+    # install exported targets
+    install(EXPORT NasNasTargets DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/NasNas NAMESPACE NasNas::)
+endfunction()
