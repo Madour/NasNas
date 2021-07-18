c90cd48ccf51ce79098a745d34eae1cb5c13723e
Modar Nasser <modar1999@gmail.com>
2021-07-18 12:56:48 +0200

    can now customize easing functions

--- examples/tweening/main.cpp
+++ examples/tweening/main.cpp
@@ -6,9 +6,6 @@
 
 #include <NasNas/Core.hpp>
 #include <NasNas/Tween.hpp>
-#include <array>
-#include <chrono>
-#include <thread>
 
 class Game : public ns::App {
     std::vector<ns::tween::EasingFunction> m_functions;
@@ -21,28 +18,34 @@ public:
         auto& camera = createCamera("main", 0);
         camera.lookAt(scene);
 
+        auto* text = new sf::Text("Press R to replay", ns::Arial::getFont(), 24);
+        text->setPosition(640-text->getGlobalBounds().width/2, 10);
+        scene.getDefaultLayer().add(text);
+
         m_functions = {
-            ns::tween::easing::linear,
-            ns::tween::easing::sinusoidalIntOut,
-            ns::tween::easing::quadraticInOut,
-            ns::tween::easing::bounceOut,
-            ns::tween::easing::elasticOut,
-            ns::tween::easing::backIn,
-            ns::tween::easing::backInOut
+            ns::easing::linear,
+            //ns::easing::sinusoidalIntOut,
+            //ns::easing::quadraticInOut,
+            ns::easing::custom::backIn<30>,
+            ns::easing::custom::backIn2<3>,
+            ns::easing::backInOut,
+            ns::easing::custom::backInOut2<3>,
+            ns::easing::bounceOut,
+            ns::easing::elasticOut,
         };
         m_balls.resize(m_functions.size());
         m_tweens.resize(m_functions.size());
 
         auto ball_radius = 25.f;
-        auto d = 5.f;
-        auto s = 100.f;
-        auto e = 900.f;
+        auto d = 2.f;
+        auto s = 300.f;
+        auto e = 1000.f;
 
         for (unsigned i = 0; i < m_functions.size(); ++i) {
             m_balls[i].setRadius(ball_radius);
             m_balls[i].setFillColor(sf::Color::Red);
             m_balls[i].setOrigin(ball_radius, ball_radius);
-            m_balls[i].setPosition(50, float(50 + 100*i));
+            m_balls[i].setPosition(50, float(80 + 100*i));
             scene.getDefaultLayer().add(m_balls[i]);
 
             m_tweens[i].from(s).to(e).during(d).with(m_functions[i]).apply([this, i](float v){
--- include/NasNas/tween/Tween.hpp
+++ include/NasNas/tween/Tween.hpp
@@ -1,66 +1,16 @@
 // Created by Modar Nasser on 17/07/2021.
 
-#include <SFML/Graphics.hpp>
-#include <functional>
 #include <utility>
+#include <functional>
+
+#include <SFML/Graphics.hpp>
+
+#include "NasNas/tween/Easing.hpp"
 
 #pragma once
 
 namespace ns::tween {
     using EasingFunction = std::function<float(float)>;
-
-    namespace easing {
-        auto linear(float t) -> float {
-            return t;
-        };
-
-        auto quadraticInOut(float t) -> float {
-            if (t < 0.5f)
-                return 2 * t * t;
-            return 1 - (-2*t + 2) * (-2*t + 2) / 2.f;
-        };
-
-        auto sinusoidalIntOut(float t) -> float {
-            return -std::cos(ns::PI * t) / 2.f + 0.5f;
-        }
-
-        auto bounceOut(float t) -> float {
-            if (t < 1 / 2.75f) {
-                return 7.5625f * t * t;
-            } else if (t < 2.0f / 2.75f) {
-                t -= 1.5f / 2.75f;
-                return 7.5625f * t * t + .75f;
-            } else if (t < 2.5f / 2.75f) {
-                t -= 2.25f / 2.75f;
-                return 7.5625f * t * t + .9375f;
-            } else {
-                t -= 2.625f / 2.75f;
-                return 7.5625f * t * t + .984375f;
-            }
-        }
-
-        auto elasticOut(float t) -> float {
-            const float c4 = (2 * ns::PI) / 5;
-            if (t == 0 || t == 1)
-                return t;
-            return std::pow(2.f, -10 * t) * std::sin((10 * t - 0.75f) * c4) + 1;
-        }
-
-        auto backIn(float t) -> float {
-            const float c1 = 1.90158f;
-            const float c3 = c1 + 1;
-            return c3 * t * t * t - c1 * t * t;
-        }
-
-        auto backInOut(float t) -> float {
-            const float c1 = 1.90158f;
-            const float c2 = c1 * 1.525f;
-
-            return t < 0.5
-                   ? (pow(2 * t, 2) * ((c2 + 1) * 2 * t - c2)) / 2
-                   : (pow(2 * t - 2, 2) * ((c2 + 1) * (t * 2 - 2) + c2) + 2) / 2;
-        }
-    }
 
     class Tween {
         std::function<void(float)> m_onstep;
@@ -72,11 +22,12 @@ namespace ns::tween {
         bool m_first_end = true;
         sf::Clock m_clock;
         inline auto interpolate(float x) const -> float { return (m_to - m_from) * x + m_from; };
+
     public:
         explicit Tween() = default;
         auto apply(std::function<void(float)> onStep) -> Tween& { m_onstep = std::move(onStep); return *this; };
         auto from(float f) -> Tween& { m_from = f; return *this; };
-        auto to(float t) -> Tween& { m_to =t; return *this; };
+        auto to(float t) -> Tween& { m_to = t; return *this; };
         auto during(float duration) -> Tween& { m_duration = duration; return *this; };
         auto with(std::function<float(float)> fn) -> Tween& { m_function = std::move(fn); return *this; };
         void restart() { m_time = 0; m_first_end = true; m_clock.restart();}
