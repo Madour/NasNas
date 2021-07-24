7ab18ecc484392cc0dfcc854bd67e4f83915b573
Modar Nasser <modar1999@gmail.com>
2021-07-24 10:30:16 +0200

    merge from and to fix delays and add after method for initial delay

--- include/NasNas/tween/Tween.hpp
+++ include/NasNas/tween/Tween.hpp
@@ -1,77 +1,108 @@
 // Created by Modar Nasser on 17/07/2021.
 
-#include <utility>
-#include <functional>
-
-#include <SFML/Graphics.hpp>
-
-#include "NasNas/tween/Easing.hpp"
-
 #pragma once
 
-#include <iostream>
+#include <utility>
+#include <functional>
+#include <SFML/Graphics.hpp>
+#include "NasNas/tween/Easing.hpp"
 
 namespace ns::tween {
     using EasingFunction = std::function<float(float)>;
+    using CallbackFunction = std::function<void(float)>;
 
     class Tween {
     public:
         std::vector<float> m_starts;
         std::vector<float> m_ends;
-        std::vector<float> m_durations;
-        std::vector<float> m_delays;
-        std::vector<EasingFunction> m_easing_fns;
-        std::vector<std::function<void(float)>> m_on_step_cbs;
+        std::vector<float> m_durations = {1.f};
+        std::vector<float> m_delays = {0.f};
+        std::vector<EasingFunction> m_easing_fns = {easing::linear};
+        std::vector<CallbackFunction> m_on_step_cbs = {[](float){}};
         unsigned m_index = 0;
+        float m_initial_delay = 0.f;
         float m_current_delay = 0.f;
-        float m_time = 0.f;
+        bool m_first_run = true;
         sf::Clock m_clock;
         bool m_loop = false;
         inline auto interpolate(unsigned i, float x) const -> float { return (m_ends[i] - m_starts[i]) * x + m_starts[i]; };
-
+        void emplaceAnimation() {
+            m_durations.emplace_back(1.f);
+            m_easing_fns.empty() ?
+                m_easing_fns.emplace_back(easing::linear) :
+                m_easing_fns.emplace_back(m_easing_fns.back());
+            m_on_step_cbs.empty() ?
+                m_on_step_cbs.emplace_back() :
+                m_on_step_cbs.emplace_back(m_on_step_cbs.back());
+            m_delays.emplace_back(0.f);
+        }
     public:
         explicit Tween() = default;
 
+        void clear() {
+            m_starts.clear();
+            m_ends.clear();
+            m_durations = {1.f};
+            m_delays = {1.f};
+            m_easing_fns = {easing::linear};
+            m_on_step_cbs = {[](float){}};
+            m_index = 0;
+            m_initial_delay = 0.f;
+            m_current_delay = 0.f;
+            m_clock.restart();
+            m_loop = false;
+        }
 
-        auto from(float f) -> Tween& {
-            m_starts.emplace_back(f);
+        auto after(float delay) -> Tween& {
+            m_initial_delay = delay;
             return *this;
         }
 
-        auto to(float t) -> Tween& {
-            if (m_ends.size() + 1 > m_starts.size()) {
-                m_starts.emplace_back(m_ends.back());
+        auto from_to(float s, float e) -> Tween& {
+            if (!m_starts.empty()) {
+                m_index++;
+                emplaceAnimation();
             }
-            if (m_ends.size() + 1 > m_delays.size()) {
-                m_delays.emplace_back(0.f);
-            }
-            m_ends.emplace_back(t);
+            m_starts.emplace_back(s);
+            m_ends.emplace_back(e);
             return *this;
-        };
+        }
+
+        auto to(float e) -> Tween& {
+            if (!m_starts.empty()) {
+                m_index++;
+                m_starts.emplace_back(m_ends.back());
+                emplaceAnimation();
+                m_ends.emplace_back(e);
+            }
+            return *this;
+        }
 
         auto during(float duration) -> Tween& {
-            m_durations.emplace_back(duration);
+            m_durations[m_index] = duration;
             return *this;
         };
 
-        auto apply(std::function<void(float)> on_step_callback) -> Tween& {
-            m_on_step_cbs.emplace_back(std::move(on_step_callback));
+        auto apply(CallbackFunction on_step_callback) -> Tween& {
+            m_on_step_cbs[m_index] = std::move(on_step_callback);
             return *this;
         };
 
         auto with(const std::function<float(float)>& fn) -> Tween& {
-            m_easing_fns.emplace_back(fn);
+            m_easing_fns[m_index] = fn;
             return *this;
         };
 
         auto delay(float delay) -> Tween& {
-            m_delays.emplace_back(delay);
+            m_delays[m_index] = delay;
             return *this;
         }
 
         void restart() {
-            m_time = 0;
+            m_index = 0;
+            m_current_delay = m_initial_delay;
             m_clock.restart();
+            m_on_step_cbs[0](m_starts[0]);
         };
 
         auto loop() -> Tween& {
@@ -84,11 +115,9 @@ namespace ns::tween {
         };
 
         auto step() -> float {
-            static bool first = true;
-            if (first) {
-                m_current_delay = m_delays[0];
-                m_clock.restart();
-                first = false;
+            if (m_first_run) {
+                restart();
+                m_first_run = false;
             }
 
             if (m_current_delay > 0.f) {
@@ -102,34 +131,27 @@ namespace ns::tween {
 
             if (ended()) {
                 if (m_loop) {
-                    m_index = 0;
-                    m_current_delay = m_delays[0];
-                    m_clock.restart();
-                    m_on_step_cbs[0](m_starts[0]);
+                    restart();
                     return 0.f;
-                }
-                else
+                } else
                     return 1.f;
             }
 
-            m_time = m_clock.getElapsedTime().asSeconds();
-            auto pt = m_time / m_durations[m_index];
+            auto pt = m_clock.getElapsedTime().asSeconds() / m_durations[m_index];
 
-            auto& easing_fn = m_index >= m_easing_fns.size() ? m_easing_fns.back() : m_easing_fns[m_index];
-            auto& callback = m_index >= m_on_step_cbs.size() ? m_on_step_cbs.back() : m_on_step_cbs[m_index];
+            auto& easing_fn = m_easing_fns[m_index];
+            auto& callback = m_on_step_cbs[m_index];
 
             if (pt < 1.f)
                 callback(interpolate(m_index, easing_fn(pt)));
             else {
                 callback(m_ends[m_index]);
-                m_time = 0;
                 m_clock.restart();
-                m_index += 1;
                 m_current_delay = m_delays[m_index];
-                std::cout << m_index << " " << m_current_delay << std::endl;
-
+                m_index += 1;
             }
             return std::clamp(pt, 0.f, 1.f);
         }
     };
+
 }
