e6b0d2b89837d5bc1dc2668691bd44bbec8badc5
Modar Nasser <modar1999@gmail.com>
2021-07-24 21:51:58 +0200

    sinusoidal + exponential + polynomial easings move template definitions to .tpp file

--- include/NasNas/tween/Easing.hpp
+++ include/NasNas/tween/Easing.hpp
@@ -9,60 +9,48 @@ namespace ns {
 
         static auto quadraticInOut(float t) -> float;
 
-        static auto sinusoidalIntOut(float t) -> float;
+        static auto sinusoidalIn(float t) -> float;
+        static auto sinusoidalOut(float t) -> float;
+        static auto sinusoidalInOut(float t) -> float;
+
+        static auto exponentialIn(float t) -> float;
+        static auto exponentialOut(float t) -> float;
+        static auto exponentialInOut(float t) -> float;
 
         static auto bounceIn(float t) -> float;
         static auto bounceOut(float t) -> float;
+
+        static auto backIn(float t) -> float;
+        static auto backOut(float t) -> float;
+        static auto backInOut(float t) -> float;
+
+        static auto backIn2(float t) -> float;
+        static auto backOut2(float t) -> float;
+        static auto backInOut2(float t) -> float;
 
         static auto elasticIn(float t) -> float;
         static auto elasticOut(float t) -> float;
         static auto elasticInOut(float t) -> float;
 
-        static auto backIn(float t) -> float;
-        static auto backInOut(float t) -> float;
-
         struct custom {
-            template <unsigned C1>
-            static auto backIn(float t) -> float;
+            template <unsigned Degree> static auto polynomialIn(float t) -> float;
+            template <unsigned Degree> static auto polynomialOut(float t) -> float;
+            template <unsigned Degree> static auto polynomialInOut(float t) -> float;
 
-            template <unsigned C1>
-            static auto backIn2(float t) -> float;
+            template <unsigned Pull> static auto backIn(float t) -> float;
+            template <unsigned Pull> static auto backOut(float t) -> float;
+            template <unsigned Pull> static auto backInOut(float t) -> float;
 
-            template <unsigned C1>
-            static auto backInOut2(float t) -> float;
+            template <unsigned Pull> static auto backIn2(float t) -> float;
+            template <unsigned Pull> static auto backOut2(float t) -> float;
+            template <unsigned Pull> static auto backInOut2(float t) -> float;
 
-            template <unsigned Ondulation>//, unsigned Attenuation>
-            static auto elasticOut(float t) -> float;
+            template <unsigned Ondulation> static auto elasticIn(float t) -> float;
+            template <unsigned Ondulation> static auto elasticOut(float t) -> float;
+            template <unsigned Ondulation> static auto elasticInOut(float t) -> float;
         };
     };
-
-    template <unsigned C1>
-    auto easing::custom::backIn(float t) -> float {
-        const float c1 = C1/10.f;
-        const float c3 = c1 + 1.f;
-        return c3 * t * t * t - c1 * t * t;
-    }
-    template <unsigned C>
-    auto easing::custom::backIn2(float t) -> float {
-        const float c = C/10.f;
-        return c * t*t - (c - 1.f) * t;
-    }
-    template <unsigned C>
-    auto easing::custom::backInOut2(float t) -> float {
-        const float c = C/10.f;
-        if (t < 0.5f) {
-            return c * (1.4142f*t)*(1.4142f*t) - (c - 1.f) * t;
-        }
-        t -= 1;
-        return -c * (1.4142f*t)*(1.4142f*t) - (c - 1.f) * t + 1;
-    }
-
-    template <unsigned Ondulation>
-    auto easing::custom::elasticOut(float t) -> float {
-        const float ondul = Ondulation / 10.f;
-        if (t == 0 || t == 1)
-            return t;
-        return std::pow(2.f, -10 * t) * std::sin(2 * ns::PI * (ondul * t - 1.25f)) + 1;
-    }
-
 }
+
+#include "NasNas/tween/Easing.tpp"
+
--- src/NasNas/tween/Easing.cpp
+++ src/NasNas/tween/Easing.cpp
@@ -15,8 +15,29 @@ auto ns::easing::quadraticInOut(float t) -> float {
     return 1 - (-2*t + 2) * (-2*t + 2) / 2.f;
 };
 
-auto ns::easing::sinusoidalIntOut(float t) -> float {
+auto ns::easing::sinusoidalIn(float t) -> float {
+    return 1 - std::cos(t * ns::PI / 2.f);
+};
+auto ns::easing::sinusoidalOut(float t) -> float {
+    return std::sin(t * ns::PI / 2.f);
+};
+auto ns::easing::sinusoidalInOut(float t) -> float {
     return -std::cos(ns::PI * t) / 2.f + 0.5f;
+}
+
+auto ns::easing::exponentialIn(float t) -> float {
+    if (t == 0) return 0;
+    return std::pow(2.f, 10 * t - 10);
+}
+auto ns::easing::exponentialOut(float t) -> float {
+    return 1 - exponentialIn(1 - t);
+}
+auto ns::easing::exponentialInOut(float t) -> float {
+    if (t == 0 || t == 1)
+        return t;
+    if (t < 0.5f)
+        return std::pow(2.f, 20 * t - 10) / 2.f;
+    return 1 - std::pow(2.f,-20 * t + 10) / 2.f;
 }
 
 auto ns::easing::bounceIn(float t) -> float {
@@ -24,38 +45,47 @@ auto ns::easing::bounceIn(float t) -> float {
 }
 
 auto ns::easing::bounceOut(float t) -> float {
+    float c = 9.5625f;
     if (t < 1 / 2.75f) {
-        return 7.5625f * t * t;
+        return (c-1.f) * t * t;
     } else if (t < 2.0f / 2.75f) {
         t -= 1.5f / 2.75f;
-        return 7.5625f * t * t + .75f;
+        return c * t * t + .75f;
     } else if (t < 2.5f / 2.75f) {
         t -= 2.25f / 2.75f;
-        return 7.5625f * t * t + .9375f;
+        return c * t * t + .9375f;
     } else {
         t -= 2.625f / 2.75f;
-        return 7.5625f * t * t + .984375f;
+        return (c+1.f) * t * t + .984375f;
     }
-}
-
-auto ns::easing::elasticIn(float t) -> float {
-    return 1.f - elasticOut(1 - t);
-}
-
-auto ns::easing::elasticOut(float t) -> float {
-    return custom::elasticOut<100>(t);
 }
 
 auto ns::easing::backIn(float t) -> float {
     return custom::backIn<20>(t);
 }
-
+auto ns::easing::backOut(float t) -> float {
+    return custom::backOut<20>(t);
+}
 auto ns::easing::backInOut(float t) -> float {
+    return custom::backInOut<20>(t);
+}
 
-    const float c1 = 2.f;
-    const float c2 = c1 * 1.525f;
+auto ns::easing::backIn2(float t) -> float {
+    return custom::backIn2<20>(t);
+}
+auto ns::easing::backOut2(float t) -> float {
+    return custom::backOut2<20>(t);
+}
+auto ns::easing::backInOut2(float t) -> float {
+    return custom::backInOut2<20>(t);
+}
 
-    if (t < 0.5f)
-        return 2 * t * t * ((2 * t * c2 + 2 * t) - c2);
-    return 2 * (t*t - 2*t + 1) * ((c2 + 1) * (t * 2 - 2) + c2) + 1;
+auto ns::easing::elasticIn(float t) -> float {
+    return custom::elasticIn<20>(t);
+}
+auto ns::easing::elasticOut(float t) -> float {
+    return custom::elasticOut<20>(t);
+}
+auto ns::easing::elasticInOut(float t) -> float {
+    return custom::elasticInOut<20>(t);
 }
