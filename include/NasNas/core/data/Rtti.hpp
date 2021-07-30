d77576c6caba87e6bf655d8f92e852af93f9eff1
Madour <modar1999@gmail.com>
2021-07-30 18:19:37 +0200

    using std in Rtti.hpp add NS_ prefix to macro

--- include/NasNas/core/data/Rtti.hpp
+++ include/NasNas/core/data/Rtti.hpp
@@ -3,22 +3,22 @@
 #pragma once
 
 namespace rtti {
-
-#define DEFINE_HAS_METHOD(name, signature)                                      \
+    using namespace std;
+#define NS_DEFINE_HAS_METHOD(name, signature)                                   \
     template<typename, typename T=signature>                                    \
     struct has_##name {};                                                       \
     template<typename C, typename Ret, typename... Args>                        \
     struct has_##name<C, Ret(Args...)> {                                        \
     private:                                                                    \
         template <typename Rt>                                                  \
-        using remove_const_ref = std::remove_const_t<std::remove_reference_t<Rt>>;\
+        using remove_const_ref = remove_const_t<remove_reference_t<Rt>>;        \
         template<typename T>                                                    \
         static constexpr auto check(T*) ->                                      \
-            std::enable_if_t<std::is_base_of_v<                                 \
-                remove_const_ref<decltype(std::declval<T>().name(std::declval<Args>()...))>,\
+            enable_if_t<is_base_of_v<                                           \
+                remove_const_ref<decltype(declval<T>().name(declval<Args>()...))>,\
                 remove_const_ref<Ret>                                           \
-            >, std::true_type>;                                                 \
-        template<typename> static constexpr std::false_type check(...);         \
+            >, true_type>;                                                      \
+        template<typename> static constexpr false_type check(...);              \
         using type = decltype(check<C>(0));                                     \
     public:                                                                     \
         static constexpr bool value = type::value;                              \
@@ -26,8 +26,8 @@ namespace rtti {
     template <typename C, typename T=signature>                                 \
     inline constexpr bool has_##name##_v = has_##name<C, T>::value
 
-    DEFINE_HAS_METHOD(getPosition, sf::Vector2f());
-    DEFINE_HAS_METHOD(getGlobalBounds, sf::FloatRect());
-    DEFINE_HAS_METHOD(getBounds, sf::FloatRect());
-    DEFINE_HAS_METHOD(update, void());
+    NS_DEFINE_HAS_METHOD(getPosition, sf::Vector2f());
+    NS_DEFINE_HAS_METHOD(getGlobalBounds, sf::FloatRect());
+    NS_DEFINE_HAS_METHOD(getBounds, sf::FloatRect());
+    NS_DEFINE_HAS_METHOD(update, void());
 }
