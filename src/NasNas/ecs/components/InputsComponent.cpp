f65edc64f04592123a36327672f810ced3b90ed7
Modar Nasser <modar1999@gmail.com>
2021-08-17 22:22:13 +0200

    update inputs component

--- src/NasNas/ecs/components/InputsComponent.cpp
+++ src/NasNas/ecs/components/InputsComponent.cpp
@@ -1,43 +1,39 @@
-/**
-* Created by Modar Nasser on 24/06/2020.
-**/
-
+// Created by Modar Nasser on 17/08/2021.
 
 #include "NasNas/core/Inputs.hpp"
-#include "NasNas/ecs/InputsComponent.hpp"
-#include "NasNas/ecs/BaseEntity.hpp"
+#include "NasNas/ecs/components/InputsComponent.hpp"
 
 using namespace ns;
 using namespace ns::ecs;
 
-old::InputsComponent::InputsComponent() :
-m_capture_input(true)
-{}
-
-void old::InputsComponent::reset() {
+void InputsComponent::reset() {
     m_inputs.clear();
     m_onpress.clear();
     m_onrelease.clear();
 }
 
-void old::InputsComponent::bind(sf::Keyboard::Key key, const std::function<void()>& function) {
+void InputsComponent::bind(sf::Keyboard::Key key, const std::function<void()>& function) {
     m_inputs[key] = function;
 }
 
-void old::InputsComponent::onPress(sf::Keyboard::Key key, const std::function<void()>& function) {
+void InputsComponent::onPress(sf::Keyboard::Key key, const std::function<void()>& function) {
     m_onpress[key] = function;
 }
 
-void old::InputsComponent::onRelease(sf::Keyboard::Key key, const std::function<void()>& function) {
+void InputsComponent::onRelease(sf::Keyboard::Key key, const std::function<void()>& function) {
     m_onrelease[key] = function;
 }
 
-void old::InputsComponent::setCaptureInput(bool capture_input) {
-    m_capture_input = capture_input ;
+void InputsComponent::enable() {
+    m_enabled = true;
 }
 
-void old::InputsComponent::update() {
-    if (m_capture_input) {
+void InputsComponent::disable() {
+    m_enabled = false;
+}
+
+void InputsComponent::update() {
+    if (m_enabled) {
         for (auto it = ns::Inputs::getKeysDown().begin(); it < ns::Inputs::getKeysDown().end(); ++it) {
             for (const auto& [key, fn] : m_inputs)
                 if (key == *it)
@@ -54,5 +50,4 @@ void old::InputsComponent::update() {
                 fn();
         }
     }
-
 }
