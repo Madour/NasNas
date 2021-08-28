9bd7c74079f6d3d4843ea0e8a6b566a9d9e501b3
Modar Nasser <modar1999@gmail.com>
2021-08-28 17:35:49 +0200

    add ns::ecs::System class

--- include/NasNas/ecs/new/Types.hpp
+++ include/NasNas/ecs/new/Types.hpp
@@ -3,6 +3,33 @@
 
 namespace ns::ecs {
     using Entity = unsigned long;
+
+    template <typename... TComps>
+    class System {
+        using FunctionType = std::function<void(TComps&...)>;
+    public:
+        System() = default;
+
+        explicit System(FunctionType fn) {
+            set(fn);
+        }
+
+        auto operator=(FunctionType fn) -> System<TComps...>& {
+            set(fn);
+            return *this;
+        }
+
+        void operator() (TComps&... comps) {
+            m_function(comps...);
+        }
+
+    private:
+        void set(FunctionType fn) {
+            m_function = std::move(fn);
+        }
+
+        FunctionType m_function {};
+    };
 }
 
 namespace ns::ecs::detail {
