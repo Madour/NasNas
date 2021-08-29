b21c3c052e3920b6fff18b7370589bc22ec232c2
Modar Nasser <modar1999@gmail.com>
2021-08-29 14:06:37 +0200

    add default inputs, physics and sprite systems update demo example to use ECS systems

---
+++ src/NasNas/ecs/DefaultSystems.cpp
@@ -0,0 +1,7 @@
+// Created by Modar Nasser on 29/08/2021.
+
+#include "NasNas/ecs/DefaultSystems.hpp"
+
+ns::ecs::System<ns::ecs::InputsComponent> ns::ecs::inputs_system{[](auto& inputs) { inputs.update(); }};
+ns::ecs::System<ns::ecs::PhysicsComponent> ns::ecs::physics_system{[](auto& physics) {physics.update();}};
+ns::ecs::System<ns::ecs::SpriteComponent> ns::ecs::sprite_system{[](auto& sprite) {sprite.update();}};
