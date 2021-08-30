1c6ef668baa552c04f191b4f51078053871dcb58
Modar Nasser <modar1999@gmail.com>
2021-08-30 19:30:38 +0200

    create platformer example

--- examples/assets/tsxs/tileset.tsx
+++ examples/assets/tsxs/tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="tileset" tilewidth="16" tileheight="16" tilecount="322" columns="23">
+<tileset version="1.5" tiledversion="1.7.1" name="tileset" tilewidth="16" tileheight="16" tilecount="322" columns="23">
  <properties>
   <property name="tileset_prop" type="float" value="5.45"/>
  </properties>
