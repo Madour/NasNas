0c10cd40195be064f90e3da424923efd0898663b
Modar Nasser <modar1999@gmail.com>
2021-08-30 15:08:09 +0200

    PhysicsComponent update update demo example : fix collision resolution + gravity

--- src/NasNas/ecs/components/ColliderComponent.cpp
+++ src/NasNas/ecs/components/ColliderComponent.cpp
@@ -6,11 +6,11 @@
 
 using namespace ns::ecs;
 
-auto AABBColliderComponent::getBounds() const -> sf::FloatRect {
+auto AABBColliderComponent::getBounds() const -> ns::FloatRect {
     return {position-size/2.f, size};
 }
 
-auto CircleColliderComponent::getBounds() const -> sf::FloatRect {
+auto CircleColliderComponent::getBounds() const -> ns::FloatRect {
     sf::Vector2f size{radius, radius};
     return {position-size/2.f, size};
 }
