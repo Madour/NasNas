7a8af03f8e441db5bea3363ec1cef9a8db364b09
Modar Nasser <modar1999@gmail.com>
2021-09-02 11:25:16 +0200

    add torque and angular velocity to PhysicsComponent update examples

--- examples/sandbox/Game.cpp
+++ examples/sandbox/Game.cpp
@@ -12,14 +12,6 @@ ns::App("NasNas demo", {640, 360}, 2, 60, 60) {
     //------------ Game Objects creation ------------------------------------------------
     // load tiled map from file
     this->tiled_map.loadFromFile("assets/test_map.tmx");
-    for (auto& rect : this->tiled_map.getGroupLayer("FrontGroup").getObjectLayer("collisions").allRectangles()) {
-        auto ent = ns::Ecs.create();
-        auto& transform = ns::Ecs.attach<ns::ecs::Transform>(ent);
-        auto& collider = ns::Ecs.attach<ns::ecs::AABBCollider>(ent);
-        transform.setPosition(rect.x, rect.y);
-        collider.size = {rect.width, rect.height};
-        collider.position = {rect.width/2.f, rect.height/2.f};
-    }
 
     // generate 100 random octogons
     auto colors = std::vector<sf::Color>{
@@ -154,7 +146,6 @@ ns::App("NasNas demo", {640, 360}, 2, 60, 60) {
     this->palette_shader->setUniform("threshold", 0.25f);
     this->setShader(palette_shader);
     this->toggleShader();
-
 }
 
 void Game::onEvent(const sf::Event& event) {
@@ -202,66 +193,10 @@ void Game::update() {
     ns::Ecs.run(ns::ecs::inputs_system);
     // run the default physics systems (adds force to velocity and apply friction)
     ns::Ecs.run(ns::ecs::physics_system);
-    // run a custom system that apply gravity to all physics components
-    ns::Ecs.run<ns::ecs::Physics>([&](auto& physics) {
-        physics.applyForce(physics.getMass() * GRAVITY / float(ns::Settings::getConfig().update_rate));
-    });
     // run a custom system that updates transformable components according to physics velocity
     ns::Ecs.run<ns::ecs::Transform, ns::ecs::Physics>([](auto& transform, auto& physics) {
-        transform.move(physics.getVelocity());
+        transform.move(physics.linear_velocity);
     });
-
-    // resolve collisions using Ecs view to get entities with given components
-    auto colls_view = ns::Ecs.view<ns::ecs::AABBCollider, ns::ecs::Transform>();
-    colls_view.for_each_pair([&](auto ent1, auto ent2) {
-        auto& coll1 = colls_view.get<ns::ecs::AABBCollider>(ent1);
-        auto& coll2 = colls_view.get<ns::ecs::AABBCollider>(ent2);
-
-        // no need to solve collision for 2 static colliders
-        if (!coll1.dynamic && !coll2.dynamic)
-            return;
-
-        auto& tr1 = colls_view.get<ns::ecs::Transform>(ent1);
-        auto& tr2 = colls_view.get<ns::ecs::Transform>(ent2);
-
-        auto bounds1 = tr1.getTransform().transformRect(coll1.getBounds());
-        auto bounds2 = tr2.getTransform().transformRect(coll2.getBounds());
-
-        sf::FloatRect intersect;
-        if (bounds1.intersects(bounds2, intersect)) {
-            // resolve first collider
-            if (coll1.dynamic) {
-                // X axis collision
-                if (intersect.width < intersect.height) {
-                    ns::Ecs.get<ns::ecs::Physics>(ent1).setVelocityX(0);
-                    float sign = (bounds1.left < bounds2.left ? -1.f : 1.f);
-                    tr1.move(sign * intersect.width, 0);
-                }
-                // Y axis collision
-                else {
-                    ns::Ecs.get<ns::ecs::Physics>(ent1).setVelocityY(0);
-                    float sign = (bounds1.top < bounds2.top ? -1.f : 1.f);
-                    tr1.move(0, sign * intersect.height);
-                }
-            }
-            // resolve second collider
-            if (coll2.dynamic) {
-                // X axis collision
-                if (intersect.width < intersect.height) {
-                    ns::Ecs.get<ns::ecs::Physics>(ent2).setVelocityX(0);
-                    float sign = (bounds2.left < bounds1.left ? -1.f : 1.f);
-                    tr2.move(sign * intersect.width, 0);
-                }
-                // Y axis collision
-                else {
-                    ns::Ecs.get<ns::ecs::Physics>(ent2).setVelocityY(0);
-                    float sign = (bounds2.top < bounds1.top ? -1.f : 1.f);
-                    tr2.move(0, sign * intersect.height);
-                }
-            }
-        }
-    });
-
     // run the default sprite component to update sprite animation
     ns::Ecs.run(ns::ecs::sprite_system);
 
--- include/NasNas/ecs/components/PhysicsComponent.hpp
+++ include/NasNas/ecs/components/PhysicsComponent.hpp
@@ -11,50 +11,30 @@ namespace ns::ecs {
 
     class PhysicsComponent {
     public:
-        explicit PhysicsComponent(const sf::Vector2f& acceleration= {0, 0}, float mass=1.f, const sf::Vector2f& friction={1.f, 1.f});
+        explicit PhysicsComponent(float mass=1.f, const sf::Vector2f& linear_damping={1.f, 1.f}, float angular_damping=1.f);
 
-        auto getMass() const -> float;
-        void setMass(float mass);
+        float mass = 1.f;
 
-        auto getVelocity() const -> const sf::Vector2f&;
-        void setVelocity(const sf::Vector2f& velocity);
-        void setVelocity(float velocity_x, float velocity_y);
-        void setVelocityX(float velocity_x);
-        void setVelocityY(float velocity_y);
+        sf::Vector2f linear_velocity = {0.f, 0.f};
+        sf::Vector2f linear_damping = {1.f, 1.f};
+
+        float angular_velocity = 0.f;
+        float angular_damping = 1.f;
 
         void applyForce(sf::Vector2f force);
-
-        auto getAcceleration() const -> const sf::Vector2f&;
-        void setAcceleration(const sf::Vector2f& acceleration);
-        void setAcceleration(float acceleration_x, float acceleration_y);
-        void setAccelerationX(float acceleration_x);
-        void setAccelerationY(float acceleration_y);
+        void applyTorque(float torque);
 
         auto getDirection() const -> sf::Vector2f;
-        void setDirection(const sf::Vector2f& direction);
-        void setDirection(float direction_x, float direction_y);
-        void setDirectionX(float direction_x);
-        void setDirectionY(float direction_y);
-
-        auto getDirectionAngle() const -> float;
-        void setDirectionAngle(float angle);
-
-        auto getDirectionMagnitude() const -> float;
-        void setDirectionMagnitude(float magnitude);
+        auto getAngle() const -> float;
 
         auto getMomentum() const -> sf::Vector2f;
 
         void update();
 
     private:
-        float m_mass;
-        sf::Vector2f m_direction = {0.f, 0.f};
-        float m_direction_angle = 0.f;
-        float m_direction_magnitude = 1.f;
-        sf::Vector2f m_velocity = {0.f, 0.f};
-        sf::Vector2f m_acceleration;
         sf::Vector2f m_forces;
-        sf::Vector2f m_friction;
+        float m_torque = 0.f;
+        float m_angle = 0.f;
     };
 
     using Physics = PhysicsComponent;
