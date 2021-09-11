c237dcadc70029b4800b284e22fd00a7307c898b
Madour <modar1999@gmail.com>
2021-09-11 11:42:16 +0200

    android libs installation support

--- examples/android/app/src/main/jni/Android.mk
+++ examples/android/app/src/main/jni/Android.mk
@@ -5,17 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := NasNas-example
 
 LOCAL_SRC_FILES := main.cpp
-LOCAL_SRC_FILES += $(wildcard ${LOCAL_PATH}/../../../../../../src/NasNas/core/*.cpp)
-LOCAL_SRC_FILES += $(wildcard ${LOCAL_PATH}/../../../../../../src/NasNas/core/**/*.cpp)
-LOCAL_SRC_FILES += $(wildcard ${LOCAL_PATH}/../../../../../../src/NasNas/ecs/*.cpp)
-LOCAL_SRC_FILES += $(wildcard ${LOCAL_PATH}/../../../../../../src/NasNas/ecs/**/*.cpp)
-LOCAL_SRC_FILES += $(wildcard ${LOCAL_PATH}/../../../../../../src/NasNas/thirdparty/*.cpp)
-LOCAL_SRC_FILES += $(wildcard ${LOCAL_PATH}/../../../../../../src/NasNas/tilemapping/*.cpp)
-LOCAL_SRC_FILES += $(wildcard ${LOCAL_PATH}/../../../../../../src/NasNas/tween/*.cpp)
-LOCAL_SRC_FILES += $(wildcard ${LOCAL_PATH}/../../../../../../src/NasNas/ui/*.cpp)
 
 LOCAL_C_INCLUDES += ${LOCAL_PATH}/include
-LOCAL_C_INCLUDES += ${LOCAL_PATH}/../../../../../../include
 
 LOCAL_SHARED_LIBRARIES := sfml-system-d
 LOCAL_SHARED_LIBRARIES += sfml-window-d
@@ -24,8 +15,11 @@ LOCAL_SHARED_LIBRARIES += sfml-audio-d
 LOCAL_SHARED_LIBRARIES += sfml-network-d
 LOCAL_SHARED_LIBRARIES += sfml-activity-d
 LOCAL_SHARED_LIBRARIES += openal
+
+LOCAL_SHARED_LIBRARIES += NasNas-d
+
 LOCAL_WHOLE_STATIC_LIBRARIES := sfml-main-d
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,third_party/sfml)
+$(call import-module,third_party/nasnas)
---
+++ src/NasNas/Android.mk
@@ -0,0 +1,162 @@
+LOCAL_PATH := $(call my-dir)
+
+### RELEASE LIBS ###
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := sfml-graphics
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-core
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-core.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := sfml-graphics
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-ecs
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-ecs.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := NasNas-core
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-tilemapping
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-tilemapping.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := NasNas-core
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-tween
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-tween.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := NasNas-core
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-ui
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-ui.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := NasNas-core
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+
+### DEBUG LIBS ###
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-d
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-d.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := sfml-graphics-d
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-core-d
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-core-d.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := sfml-graphics-d
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-ecs-d
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-ecs-d.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := NasNas-core-d
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-tilemapping-d
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-tilemapping-d.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := NasNas-core-d
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-tween-d
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-tween-d.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := NasNas-core-d
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := NasNas-ui-d
+LOCAL_SRC_FILES := lib/$(TARGET_ARCH_ABI)/libNasNas-ui-d.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SHARED_LIBRARIES := NasNas-core-d
+
+prebuilt_path := $(call local-prebuilt-path,$(LOCAL_SRC_FILES))
+prebuilt := $(strip $(wildcard $(prebuilt_path)))
+
+ifdef prebuilt
+    include $(PREBUILT_SHARED_LIBRARY)
+endif
+
+$(call import-module,third_party/sfml)
